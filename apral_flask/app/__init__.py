0e7172ab9b57eb04a7adf7141e8f76e2d240523f
Dominic <dominicslw@gmail.com>
2021-10-31 01:47:28 -0500

    Start back-end

---
+++ apral_flask/app/__init__.py
@@ -0,0 +1,54 @@
+"""Setup at app startup"""
+import os
+import sqlalchemy
+from flask import Flask
+from yaml import load, Loader
+
+
+app = Flask(__name__)
+def init_connection_engine():
+    """ initialize database setup
+    Takes in os variables from environment if on GCP
+    Reads in local variables that will be ignored in public repository.
+    Returns:
+        pool -- a connection to GCP MySQL
+    """
+
+
+    # detect env local or gcp
+    if os.environ.get('GAE_ENV') != 'standard':
+        try:
+            variables = load(open("app.yaml"), Loader=Loader)
+        except OSError as e:
+            print("Make sure you have the app.yaml file setup")
+            os.exit()
+
+        env_variables = variables['env_variables']
+        for var in env_variables:
+            os.environ[var] = env_variables[var]
+
+    pool = sqlalchemy.create_engine(
+        sqlalchemy.engine.url.URL(
+            drivername="mysql+pymysql",
+            username=os.environ.get('MYSQL_USER'),
+            password=os.environ.get('MYSQL_PASSWORD'),
+            database=os.environ.get('MYSQL_DB'),
+            host=os.environ.get('MYSQL_HOST')
+        )
+    )
+
+    return pool
+
+
+
+db = init_connection_engine()
+
+conn = db.connect()
+result  = conn.execute("SELECT * FROM APRAL.Game where Genre = 'Action';").fetchall()
+print(result[0])
+conn.close()
+
+# To prevent from using a blueprint, we use a cyclic import
+# This also means that we need to place this import here
+# pylint: disable=cyclic-import, wrong-import-position
+from app import routes
